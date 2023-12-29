139868a5a25fc798edd5d018fc8f87e86dd0811c
Santiago Menendez <santiagomenendez@outlook.com>
2023-12-29 00:15:05 -0300

    Fix width in projects component

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -21,7 +21,7 @@
           :title="project.name"
           outlined
           tile
-          width="500px"
+          max-width="500px"
           height="100%"
           class="d-flex flex-column"
         >
@@ -64,62 +64,6 @@
         </v-card>
       </v-col>
     </v-row>
-    <!--
-    <v-card
-      class="pa-5 rounded-xl"
-    >
-      <v-row
-        class="ma-auto"
-      >
-        <v-col>
-          <div class="text-start">
-            <h3>
-              {{ $t("projects.title") }}
-            </h3>
-          </div>
-        </v-col>
-      </v-row>
-      <v-row class="ma-5">
-        <div class="d-flex flex-wrap justify-center ma-auto">
-          <v-row justify="center">
-            <v-card
-              v-for="project in projects"
-              :key="project"
-              class="px-5 mx-1 my-2"
-              outlined
-              tile
-              width="500px"
-            >
-              <v-img
-                :src="'assets/images/' + project.image"
-                class="grey darken-4"
-                width="500px"
-                height="200px"
-              />
-              <div class="text-center ma-auto">
-                <h4>{{ project.name }}</h4>
-              </div>
-              <p class="text-center ma-5">
-                {{ project.description }}
-              </p>
-              <v-divider />
-              <p class="text-end">
-                <a
-                  class="text-end"
-                  :href="project.url_github"
-                  target="_blank"
-                >
-                  <div>
-                    {{ $t("projects.url_title") }}
-                  </div>
-                </a>
-              </p>
-            </v-card>
-          </v-row>
-        </div>
-      </v-row>
-    </v-card>
-    -->
   </v-container>
 </template>
 
